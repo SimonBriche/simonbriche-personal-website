692207f4143da3031c63f3e69fb9cac5d927c5cc
Simon BRICHE <simon.briche@onprenduncafe.com>
2021-11-12 22:10:17 +0100

    reorganize server middleware

--- middlewares/https.js
+++ middlewares/https.js
@@ -1,8 +1,6 @@
 const config = require('../config');
 
 module.exports = function(req, res, next) {
-  const ua = (req.headers && req.headers['user-agent']) ? req.headers['user-agent'].toLowerCase() : undefined;
-  _logger.debug('ua', ua)
   if(config.production && !req.secure && config.application.forceSSLRedirection) {
     res.redirect(301, 'https://' + req.headers.host + req.url);
   } else {
