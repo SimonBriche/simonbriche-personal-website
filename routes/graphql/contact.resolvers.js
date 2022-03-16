64bc72ef5f8608df13a5007c45fcc0fc4059b0a6
Simon BRICHE <simon.briche@onprenduncafe.com>
2022-03-16 10:07:01 +0100

    Update email sender

--- routes/graphql/contact.resolvers.js
+++ routes/graphql/contact.resolvers.js
@@ -5,13 +5,14 @@ const resolvers = {
   Mutation: {
     sendContactMessage: async (root, {input}, context, info) => {
       const mailOptions = {
-        from: 'Contact <noreply@6pans.fr>',
+        from: `Contact <${config.mail.senderEmail}>`,
         to: config.mail.supportEmail,
         subject: `Nouveau message de contact de l'application ${config.application.url}`,
         htmlPath: './public/mails/contact',
         variables: input
       };
       
+      mailOptions.variables.subject = mailOptions.subject;
       if(mailOptions.variables.message){
         mailOptions.variables.message = mailOptions.variables.message.replace(/(?:\r\n|\r|\n)/g, '<br />');
       }
